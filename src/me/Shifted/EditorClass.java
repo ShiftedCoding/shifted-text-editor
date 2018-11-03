6a9a9e9f5e359dcb37c0c6992bbef8e4cb46d17f
ShiftedCoding <kalebr82@outlook.com>
2018-11-03 11:38:53 -0400

    Add form base

---
+++ src/me/Shifted/EditorClass.java
@@ -0,0 +1,10 @@
+package me.Shifted;
+
+import javax.swing.*;
+
+//Created on 2018-11-03 in package me.Shifted
+public class EditorClass {
+  private JTextArea editorTextField;
+  private JTabbedPane tabbedPane1;
+  private JPanel editorTextArea;
+}
